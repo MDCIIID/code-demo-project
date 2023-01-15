8ea82e87b58fa0a3c54e542317fd6253fa37043a
MDCIIID <akaryuterra@gmail.com>
2023-01-15 00:15:27 -0800

    Incremental unit test update.

--- src/components/CardComponent.test.tsx
+++ src/components/CardComponent.test.tsx
@@ -4,15 +4,34 @@ import { CardComponent } from './CardComponent';
 import { DEFAULT_CARD, Card } from '../classes/GameClasses';
 import { Suits, Ranks, SuitIcons, RankData } from '../constants/Constants';
 
-test('renders CardComponent', () => {
+describe('CardComponent', () => {
+    //Given
     let testCard:Card = DEFAULT_CARD;
-    testCard.flip();
 
+    beforeEach(() => {
+        init();
+    });
+
+    const init = () => {
+        render(<CardComponent card={DEFAULT_CARD}/>);
+    }
+
+    test('renders face down card by default', () => {
+    
+    let component = screen.getByTestId('cardComponent');
+    expect(component).toBeInTheDocument();
+    });
+
+
+    test('renders card with suit and rank when flipped face up', () => {
     render(<CardComponent card={DEFAULT_CARD}/>);
-    const component = screen.getByTestId('cardComponent');
+    testCard.flip();
+    let component = screen.getByTestId('cardComponent');
     const suit = screen.getByTestId("suit-span").innerHTML;
     const rank = screen.getByTestId("rank-span").innerHTML;
     expect(suit).toEqual(SuitIcons[Suits.spades]);
     expect(rank).toEqual(RankData[Ranks.Ace].letter);
     expect(component).toBeInTheDocument();
+    });
+
   });
